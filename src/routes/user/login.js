4d7ad87075dd4b850e88ce6f0fc35e54ad95301d
Ali Zarshenas <ali.zarshenas.2018@gmail.com>
2026-02-14 23:52:06 +0330

    same site attribute added for set cookie

--- src/routes/user/login.js
+++ src/routes/user/login.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const loginRouter = express.Router();
 const Users = require("../../database/schemas/userSchema");
-const { compareHashed } = require("../../utils/helper");
+const {compareHashed} = require("../../utils/helper");
 const createSecretToken = require("../../utils/secretToken");
 
 loginRouter.post("/auth/login", async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) return res.sendStatus(404);
-  const user = await Users.findOne({ email });
-  if (!user) return res.status(404).send("User not found");
-  if (!compareHashed(password, user.password))
-    return res.status(401).send("Wrong password");
-  const token = createSecretToken(user._id);
-  res.cookie("token", token, {
-    withCredentials: true,
-    httpOnly: false,
-    sameSite: "none",
-  });
-  res.sendStatus(200);
+    const {email, password} = req.body;
+    if (!email || !password) return res.sendStatus(404);
+    const user = await Users.findOne({email});
+    if (!user) return res.status(404).send("User not found");
+    if (!compareHashed(password, user.password))
+        return res.status(401).send("Wrong password");
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    });
+    res.sendStatus(200);
 });
 
-module.exports = { loginRouter };
+module.exports = {loginRouter};
